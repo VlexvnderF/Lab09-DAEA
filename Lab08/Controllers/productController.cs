0cefb9c04d35d9bcf09955475c8b83bfcc6bec12
Vlexvnder <alexander.flores.r@tecsup.edu.pe>
2023-05-22 21:26:02 -0500

    Agregar archivos de proyecto.

---
+++ Lab08/Controllers/productController.cs
@@ -0,0 +1,49 @@
+﻿using Lab08.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab08.Controllers
+{
+    public class productController : Controller
+    {
+        private NeptunoEntities1 db = new NeptunoEntities1();
+
+        // GET: product
+        public ActionResult Index()
+        {
+            return View(db.productos.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idProducto,nombreProducto,cantidadPorUnidad,precioUnidad,categoriaProducto")] productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.productos.Add(productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productos);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
